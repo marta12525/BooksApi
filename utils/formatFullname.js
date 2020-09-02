e6c90053212457766e630f3515f17e9f472579df
Marta Bednarz <martab2525@gmail.com>
2020-09-02 19:22:06 +0200

    change return in formatFullname

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -7,10 +7,6 @@ module.exports = (fullName) => {
 
   if (!firstName || !lastName) return false;
   return (
-    firstName[0].toUpperCase() +
-    firstName.slice(1).toLowerCase() +
-    ' ' +
-    lastName[0].toUpperCase() +
-    lastName.slice(1).toLowerCase()
+    `${firstName[0].toUpperCase()}${firstName.slice(1).toLowerCase()} ${lastName[0].toUpperCase()}${lastName.slice(1).toLowerCase()}`
   )
 };
